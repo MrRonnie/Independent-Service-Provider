8f74ee36cc686aa1eb7b8c0ca85195bb460f6ee0
MrRonnie <ronymaza55@gmail.com>
2022-04-18 01:29:31 +0600

    Basic Navbar added

--- src/Pages/Home/Client/Client.js
+++ src/Pages/Home/Client/Client.js
@@ -1,21 +1,24 @@
 import React from "react";
+import "./Client.css";
 
 const Client = ({ client }) => {
   const { name, img } = client;
   return (
-    <div className="g-5 col-sm-12 col-md-6 col-lg-4 mt-3 shadow-lg rounded">
-      <div className="card" style={{ width: "18rem" }}>
-        <img src={img} className="card-img-top" alt="..." />
+    <div className="g-5 col-sm-12 col-md-6 col-lg-4 mt-3 ">
+      <div
+        className="card shadow-lg rounded mt-3 mx-auto"
+        style={{ width: "18rem" }}
+      >
+        <img src={img} className="card-img-top w-100" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content <br />
-            <span className="text-primary cursor-pointer">See More ...</span>
+            <small>
+              <span className="text-primary cursor-pointer">See More ...</span>
+            </small>
           </p>
-          {/* <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a> */}
         </div>
       </div>
     </div>
