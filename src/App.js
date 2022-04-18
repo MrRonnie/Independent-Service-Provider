68c0c42eaabc689fa92dc88a2eac1db43b2e9891
MrRonnie <ronymaza55@gmail.com>
2022-04-18 14:31:01 +0600

    Auth redirect and singout option added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from "./Pages/Checkout/Checkout";
 import Home from "./Pages/Home/Home/Home";
 import Login from "./Pages/Login/Login/Login";
 import Register from "./Pages/Login/Register/Register";
+import RequireAuth from "./Pages/Login/RequireAuth/RequireAuth";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 import NotFound from "./Pages/Shared/NotFound/NotFound";
@@ -18,7 +19,11 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route
           path="/service/:serviceId"
-          element={<Checkout></Checkout>}
+          element={
+            <RequireAuth>
+              <Checkout></Checkout>
+            </RequireAuth>
+          }
         ></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,8 +1,16 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Header = () => {
+  const [user] = useAuthState(auth);
+
+  const handleSignOut = () => {
+    signOut(auth);
+  };
   return (
     <Navbar collapseOnSelect expand="lg" sticky="top" bg="dark" variant="dark">
       <Container>
@@ -30,9 +38,18 @@ const Header = () => {
             <Nav.Link as={Link} to="about">
               About
             </Nav.Link>
-            <Nav.Link as={Link} to="login">
-              Login
-            </Nav.Link>
+            {user ? (
+              <button
+                className="btn btn-link text-white text-decoration-none"
+                onClick={handleSignOut}
+              >
+                Sign out
+              </button>
+            ) : (
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
