7532227a9e16283f7ec337b984244238a4a726ec
MrRonnie <ronymaza55@gmail.com>
2022-04-18 11:28:38 +0600

    Create Login form and make it look ok

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import About from "./Pages/About/About";
 import Checkout from "./Pages/Checkout/Checkout";
 import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
 import Footer from "./Pages/Shared/Footer/Footer";
 import Header from "./Pages/Shared/Header/Header";
 import NotFound from "./Pages/Shared/NotFound/NotFound";
@@ -19,6 +21,8 @@ function App() {
           element={<Checkout></Checkout>}
         ></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
 
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
